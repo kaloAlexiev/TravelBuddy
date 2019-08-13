2d82f64f0b567c583381a211007534b526ec6bc8
Kaloyan Aleksiev <Kaloyan.Aleksiev@dswe.local>
2019-08-13 11:53:41 +0300

    Updates

---
+++ precache-manifest.0e5da7791ecc4f3e62cec0c538df2ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ef513eb2c6d6abe9eb8cfa636eaf9d",
+    "url": "/TravelBuddy/index.html"
+  },
+  {
+    "revision": "7714a997396c5121c3b0",
+    "url": "/TravelBuddy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "82760e3e562a61015f50",
+    "url": "/TravelBuddy/static/js/2.e71b393e.chunk.js"
+  },
+  {
+    "revision": "7714a997396c5121c3b0",
+    "url": "/TravelBuddy/static/js/main.de9d5fa7.chunk.js"
+  },
+  {
+    "revision": "b23d24b911263d0d1a2f",
+    "url": "/TravelBuddy/static/js/runtime~main.44d2ef19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TravelBuddy/static/media/logo.5d5d9eef.svg"
+  }
+]);
